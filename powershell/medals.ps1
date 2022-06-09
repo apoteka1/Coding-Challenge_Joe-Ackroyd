2910e7aebb23c4561f1b52a6fc910833764cd285
apoteka1 <joseph.ackroyd.1991@gmail.com>
2022-06-09 12:01:30 +0100

    complete MedalTable function

--- powershell/medals.ps1
+++ powershell/medals.ps1
@@ -6,7 +6,23 @@ function Build-MedalTable {
         $Medals
     )
 
-    # Add your code here
-    # Create a medal table based on the provided result list
-    # First place gives 3 points, second place 2 points and third place 1 point
+    $results = @()
+    $table = @{}
+
+    foreach ($p in $medals) {
+        $results += $p.podium
+    }
+
+    foreach ($r in $results) {
+
+        $pos = $r[0]
+        $posint = [convert]::ToInt32($pos, 10)
+        $score = (4 - $posint)
+        $country = $r.substring(2)
+
+        $table[$country] += $score
+
+    }
+
+    return $table
 }
